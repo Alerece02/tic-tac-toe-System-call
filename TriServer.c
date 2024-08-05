2c360179e11f881fa1398da863a526ffc2b8257b
Alessio Receku <alerece@Air-di-Alessio.home>
2024-08-05 18:50:13 +0200

    Controllo e salvataggio dei parametri

--- TriServer.c
+++ TriServer.c
@@ -15,8 +15,37 @@
 
 
 
-
-
 int main(int argc, char *argv[]){
 
+    //checking that the number of parameters is correct
+    if(argc != 4){
+        perror("Numero di parametri non valido!\n");
+        exit(0);
+    }
+
+    //checking that the two signs are not the same.
+    if(argv[2] == argv[3]){
+        perror("Non ci possono essere due segni uguali!");
+        exit(0);
+    }
+
+    //checking that the timer is not negative
+    if(atoi(argv[1]) < 0){
+        perror("Il timer non può essere negativo!");
+        exit(0);
+    }
+
+    //storing the passed parameters
+    int timer = atoi(argv[1]);
+    char segno1 = *argv[2];
+    char segno2 = *argv[3];
+
+
+
+
+
+    return 0;
 }
+
+
+
