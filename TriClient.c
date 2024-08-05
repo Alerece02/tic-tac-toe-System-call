c632fb206b5d0d488f7e7504e0a5e1210f3b2691
Alessio Receku <alerece@Air-di-Alessio.home>
2024-08-05 18:03:01 +0200

    Creazione dei File TriClient.c e TriServer.c

---
+++ TriClient.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+
+}
---
+++ TriServer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+}
